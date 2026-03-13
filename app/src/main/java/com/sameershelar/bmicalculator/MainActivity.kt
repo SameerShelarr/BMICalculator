c7cdb1dce86ba5a1a694b0b5ea27ae123b1ed8e3
Sameer Shelar <workingshelar@gmail.com>
2026-03-13 18:59:55 +0900

    Initial commit

---
+++ app/src/main/java/com/sameershelar/bmicalculator/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.sameershelar.bmicalculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sameershelar.bmicalculator.ui.theme.BMICalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BMICalculatorTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HomeScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            text = "Hello, healthy person!",
+            modifier = modifier
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    BMICalculatorTheme {
+        HomeScreen()
+    }
+}
---
+++ app/src/main/java/com/sameershelar/bmicalculator/ui/theme/Theme.kt
@@ -0,0 +1,59 @@
+package com.sameershelar.bmicalculator.ui.theme
+
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+// High Contrast Dark Scheme
+val HighContrastDarkColors = darkColorScheme(
+    primary = White,
+    onPrimary = Black,
+    surface = Black,
+    onSurface = White,
+    background = Black,
+    onBackground = White,
+    primaryContainer = White,
+    onPrimaryContainer = Black
+)
+
+// High Contrast Light Scheme
+val HighContrastLightColors = lightColorScheme(
+    primary = Black,
+    onPrimary = White,
+    surface = White,
+    onSurface = Black,
+    background = White,
+    onBackground = Black,
+    primaryContainer = Black,
+    onPrimaryContainer = White
+)
+
+@Composable
+fun BMICalculatorTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> HighContrastDarkColors
+        else -> HighContrastLightColors
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
