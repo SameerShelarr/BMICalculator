5aa85ea41e57e3740351be40d1849df5671f693f
Sameer Shelar <workingshelar@gmail.com>
2026-03-15 01:09:00 +0900

    Refactor ktlint configuration and add GitHub Actions for PR checks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,24 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file where you can add configuration options common to all subprojects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ktlint)
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        android.set(true)
+        ignoreFailures.set(false)
+
+        additionalEditorconfig.set(
+            mapOf("ktlint_standard_function-naming" to "disabled"),
+        )
+
+        reporters {
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.HTML)
+        }
+    }
 }
